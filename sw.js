735047562fc3b5a75f4306052262c262cddbec3b
Sayanmondal13m <sm1555524@gmail.com>
2024-12-02 08:23:24 +0530

    Initial commit

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('push', (event) => {
+    const data = event.data.json();
+    console.log('Push event received:', data);
+
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: data.icon || '/icon.png',
+    });
+});
